6d6b337e5eee18cb910305643458559d1182b51e
VasilenkoVyacheslav <vasilenko.vyacheslav@gmail.com>
2021-10-20 21:31:06 +0300

    Lesson 19

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 window.$ = window.jQuery = require('jquery');
+
+$(document).ready(function () {
+    $('.header').on('click', '.header__mobile-menu, .navigation__close', function () {
+        $('.navigation').slideToggle();
+    })
+});
+
+$(window).resize(function () {
+    if($(window).width() >= 768) {
+        $('.navigation').attr('style', '');
+    }
+});
+
+$(window).scroll(function () {
+    const header = $('.header');
+    const windowPosition = $(window).scrollTop();
+    const bannerHeight = $('#banner').outerHeight();
+
+    (windowPosition >= bannerHeight - header.outerHeight()) ? header.addClass('sticky') : header.removeClass('sticky');
+})
